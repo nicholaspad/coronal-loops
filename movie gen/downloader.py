99207710bf9d473c2807cede4121493cc836094c
Nicholas Padmanabhan <padman@kapalua.local>
2018-06-19 16:52:55 -0700

    generated first hmi video!

--- movie gen/downloader.py
+++ movie gen/downloader.py
@@ -1,30 +1,41 @@
 from sunpy.net.helioviewer import HelioviewerClient
 import cv2
 import os
+import re
 
 hv = HelioviewerClient()
 
-datasources = hv.get_data_sources()
+# datasources = hv.get_data_sources()
 
-for observatory, instruments in datasources.items():
-	for inst, detectors in instruments.items():
-		for det, measurements in detectors.items():
-			for meas, params in measurements.items():
-				print("%s %s: %d" % (observatory, params["nickname"], params["sourceId"]))
+# for observatory, instruments in datasources.items():
+# 	for inst, detectors in instruments.items():
+# 		for det, measurements in detectors.items():
+# 			for meas, params in measurements.items():
+# 				print("%s %s: %d" % (observatory, params["nickname"], params["sourceId"]))
 
 hours = 24
 fps = 10
 
-for hour in range(0, hours):
-	#for min in range (0,60):
-	# hv.download_png('2018/04/%02d' % (day), 4.8, "[13,1,100]", x0=0, y0=0, width=1024, height=1024)
-	hv.download_jp2("2016/04/17 %02d:00:00" % (hr), observatory = "SDO", instrument = "HMI", detector = "HMI", measurement = "continuum")
-	print "%.0f percent complete." % (100.0 * (hour + 1)/float(hours))
+# for hour in range(0, hours):
+# 	# for min in range (0,60):
+# 	# hv.download_png('2018/04/%02d' % (day), 4.8, "[13,1,100]", x0=0, y0=0, width=1024, height=1024)
+# 	hv.download_jp2("2016/04/17 %02d:00:00" % (hour), observatory = "SDO", instrument = "HMI", detector = "HMI", measurement = "magnetogram")
+# 	print "%.0f percent complete." % (100.0 * (hour + 1)/float(hours))
 
-folder = '/Users/LMSAL/sunpy/data'
+folder = '/Users/padman/sunpy/data'
 name = 'output.mp4'
 
-images = [img for img in os.listdir(folder) if img.endswith(".jp2")]
+images = []
+for f in os.listdir(folder):
+    if f.endswith(".jp2"):
+        images.append(f)
+
+def name2num(name):
+   m = re.search('img_(\d+)\.?.*', name)
+   return int(m.group(1),10)
+
+images.sort(key=name2num)
+
 frame = cv2.imread(os.path.join(folder, images[0]))
 height, width, layers = frame.shape
 
