dd3b8ee70472f1ff998ffb1214f1d3ff80662323
Nicholas Padmanabhan <nicholaspad@gmail.com>
2018-11-20 13:20:14 -0800

    progress

--- trace.py
+++ trace.py
@@ -18,14 +18,14 @@ img = np.array([[1,1,0,0,0,0,0,0,0,0],
 				[0,0,0,0,0,0,0,0,0,1]]).astype(float)
 
 img = np.zeros((500,500)).astype(float)
-img[247,252] = 1.
+img[247,244] = 1.
+img[249,246] = 1.
 
 xi = 249
 yi = 249
 r = 3
 
-mask = np.zeros(img.shape)
-mask[xi,yi] = 1
+EXP_MULT = 3
 
 def weightvector(fov):
 	xcm = 0
@@ -35,7 +35,7 @@ def weightvector(fov):
 
 	for y in range(len(fov)):
 		for x in range(len(fov[0])):
-			if (x-r) != 0 and (y-r) != 0:
+			if not ((x-r) == 0 and (y-r) == 0):
 				xcm += (x-r) * fov[y,x]
 				ycm += (y-r) * fov[y,x]
 				M += fov[y,x]
@@ -49,38 +49,74 @@ def weightvector(fov):
 	return (xcm, ycm)
 
 def vectorarray(vector, img):
-	dim = max(np.abs(vector[0]), np.abs(vector[1])) * 3
-	k, h = np.ogrid[-dim:dim, -dim:dim]
-	m = vector[1]/vector[0]
-	ar = (m*h == k)
+	dim = max(np.abs(vector[0]), np.abs(vector[1])) * EXP_MULT
+	_y, _x = np.ogrid[-dim:dim+1, -dim:dim+1]
+
+	if vector[0] != 0:
+		m = vector[1]/vector[0]
+		ar = (m*_x==_y)
+	else:
+		ar = (_x==vector[0])
+
 	ar = dilate(ar, np.ones((3,3)))
+
+	_x = vector[0]
+	_y = vector[1]
+
+	#align vector array
+	border = np.argwhere(ar)
+	tl = border.min(axis=0)
+	br = border.max(axis=0)
+	ar = ar[tl[0]:br[0]+1, tl[1]:br[1]+1]
 	return ar
 
 def move(vector, varray, img, mask):
 	cast = np.zeros(img.shape)
-	if vector[1]>0 and vector[0]>0:
-		# attach upper-left corner of varray to the vector tail
-		cast[xi:xi+varray.shape[0], yi:yi+varray.shape[1]] = varray
-		plt.imshow(cast,cmap="gray");plt.show()
-	elif vector[1]<0 and vector[0]>0:
-		# attach lower-left corner of varray to the vector tail
-		cast[xi-varray.shape[0]:xi, yi:yi+varray.shape[1]] = varray
-		plt.imshow(cast,cmap="gray");plt.show()
-	elif vector[1]<0 and vector[0]<0:
-		# attach lower-right corner of varray to the vector tail
-		pass
-	elif vector[1]>0 and vector[0]<0:
-		# attach upper-right corner of varray to the vector tail
-		pass
+	_x = vector[0]
+	_y = vector[1]
+
+	if _y>0 and _x>0: # down right
+		cast[yi-1:yi+varray.shape[0]-1, xi-1:xi+varray.shape[1]-1] = varray
+
+	elif _y<0 and _x>0: # up right
+		cast[yi-varray.shape[0]+1:yi+1, xi-1:xi+varray.shape[1]-1] = varray
+
+	elif _y<0 and _x<0: # up left
+		cast[yi-varray.shape[0]+1:yi+1, xi-varray.shape[1]+1:xi+1] = varray
+
+	elif _y>0 and _x<0: # down left
+		cast[yi-1:yi+varray.shape[0]-1, xi-varray.shape[1]+1:xi+1] = varray
+
+	elif _y<0 and _x==0: # up
+		cast[int(yi-EXP_MULT*np.abs(_y)):yi+1, xi] = 1.
+		cast = dilate(cast, np.ones((3,3)))
+
+	elif _y>0 and _x==0: # down
+		cast[yi:int(yi+EXP_MULT*np.abs(_y))+1, xi] = 1.
+		cast = dilate(cast, np.ones((3,3)))
+
+	elif _y==0 and _x>0: # right
+		cast[yi, xi:int(xi+EXP_MULT*np.abs(_x)+1)] = 1.
+		cast = dilate(cast, np.ones((3,3)))
+
+	elif _y==0 and _x<0: # left
+		cast[yi, int(xi-EXP_MULT*np.abs(_x)):xi+1] = 1.
+		cast = dilate(cast, np.ones((3,3)))
+
 	mask += cast
 	mask[mask>1] = 1
 	return mask
 
-fov = img[xi-r : xi+r+1, yi-r : yi+r+1]
-print fov
+space = np.zeros(img.shape)
+fov = img[yi-r:yi+r+1, xi-r:xi+r+1]
 
 vector = weightvector(fov)
-
 varray = vectorarray(vector, img)
+space = move(vector, varray, img, space)
 
-mask = move(vector, varray, img, mask)
+_x = vector[0]
+_y = vector[1]
+
+# make fov in the direction of the previous movement vector
+
+debug()
